9c2910ee1a6408fa30f3d39c58c7c63084abc7b8
sahrulmj85 <mohdsahrul@gmail.com>
2025-04-28 16:33:54 +0800

    Update multipleURL.py

--- multipleURL.py
+++ multipleURL.py
@@ -5,8 +5,19 @@ import time
 
 # List of URLs
 urls = [
-    "https://ss.shipmentlink.com/tvs2/download_txt/VTS_9.txt",
-    "https://ss.shipmentlink.com/tvs2/download_txt/CIX2_9.txt",
+"https://ss.shipmentlink.com/tvs2/download_txt/APG_9.txt",
+"https://ss.shipmentlink.com/tvs2/download_txt/AUE_W.txt",
+"https://ss.shipmentlink.com/tvs2/download_txt/CEM_E.txt",
+"https://ss.shipmentlink.com/tvs2/download_txt/CES_E.txt",
+"https://ss.shipmentlink.com/tvs2/download_txt/HTW_W.txt",
+"https://ss.shipmentlink.com/tvs2/download_txt/CMEX_9.txt",
+"https://ss.shipmentlink.com/tvs2/download_txt/MEX1_E.txt",
+"https://ss.shipmentlink.com/tvs2/download_txt/BEX_E.txt",
+"https://ss.shipmentlink.com/tvs2/download_txt/BEX2_E.txt",
+"https://ss.shipmentlink.com/tvs2/download_txt/FAL1_E.txt",
+"https://ss.shipmentlink.com/tvs2/download_txt/FAL3_E.txt",
+"https://ss.shipmentlink.com/tvs2/download_txt/PE1_1.txt",
+"https://ss.shipmentlink.com/tvs2/download_txt/CBX_W.txt",
     # Add more URLs here
 ]
 
